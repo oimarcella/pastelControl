9f865654ad2bb192a82a56987d3d712017c454ed
mlasa <amorimsadev@gmail.com>
2025-04-11 22:39:39 -0300

    adicionar link do front no cors do server

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // Vite
+    origin: ["http://localhost:5173", 'https://pastel-control.vercel.app/'], // Vite
     methods: ["GET", "POST"]
   }
 });
@@ -45,6 +45,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3001, () => {
-  console.log("Servidor rodando na porta 3001");
+const PORT = 3001
+server.listen(PORT, () => {
+  console.log("✌️😎 - Estou online ");
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
